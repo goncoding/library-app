5b23790f56247384ef724c861ed705a83f676cfb
deokgoni <doekgoni@naver.com>
2023-02-27 23:20:29 +0900

    java 01

--- src/main/java/com/group/libraryapp/LibraryAppApplication.java
+++ src/main/java/com/group/libraryapp/LibraryAppApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LibraryAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibraryAppApplication.class, args);
+
+		 SpringApplication.run(LibraryAppApplication.class, args);
 	}
 
-}
+ }
